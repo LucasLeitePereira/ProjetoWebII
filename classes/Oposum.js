080416ac676a2bd274d318b90479b6b48fdfc622
Lucas <lucasleitep15@gmail.com>
2025-05-18 21:30:42 -0300

    Inimigos morrem

--- classes/Oposum.js
+++ classes/Oposum.js
@@ -1,26 +1,27 @@
-const OPOSUM_X_VELOCITY = -20;
-const OPOSUM_JUMP_POWER = 250;
-const OPOSUM_GRAVITY = 580;
+
+const OPOSUM_X_VELOCITY = -20
+const OPOSUM_JUMP_POWER = 250
+const OPOSUM_GRAVITY = 580
 
 class Oposum {
   constructor(
-    { x, y, size, velocity = { x: OPOSUM_X_VELOCITY, y: 0 } },
-    turningDistance = 100
+    { x, y, width, height, velocity = { x: OPOSUM_X_VELOCITY, y: 0 } },
+    turningDistance = 100,
   ) {
-    this.x = x;
-    this.y = y;
-    this.width = size;
-    this.height = size;
-    this.velocity = velocity;
-    this.isOnGround = false;
-    this.isImageLoaded = false;
-    this.image = new Image();
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.velocity = velocity
+    this.isOnGround = false
+    this.isImageLoaded = false
+    this.image = new Image()
     this.image.onload = () => {
-      this.isImageLoaded = true;
-    };
-    this.image.src = "../images/OpossumSpritesheet.png";
-    this.elapsedTime = 0;
-    this.currentFrame = 0;
+      this.isImageLoaded = true
+    }
+    this.image.src = '../images/OpossumSpritesheet.png'
+    this.elapsedTime = 0
+    this.currentFrame = 0
     this.sprites = {
       run: {
         x: 0,
@@ -29,43 +30,44 @@ class Oposum {
         height: 28,
         frames: 6,
       },
-    };
-    this.currentSprite = this.sprites.run;
-    this.facing = 'right';
+    }
+    this.currentSprite = this.sprites.run
+    this.facing = 'right'
     this.hitbox = {
       x: 0,
       y: 0,
       width: 31,
       height: 23,
-    };
-    this.distanceTraveled = 0;
-    this.turningDistance = turningDistance;
+    }
+    this.distanceTraveled = 0
+    this.turningDistance = turningDistance
   }
 
   draw(c) {
-    // // Red square debug code
-    // c.fillStyle = "rgba(255, 0, 0, 0.5)";
-    // c.fillRect(this.x, this.y, this.width, this.height);
+    // Red square debug code
+    // c.fillStyle = 'rgba(255, 0, 0, 0.5)'
+    // c.fillRect(this.x, this.y, this.width, this.height)
 
-    // // Hitbox
-    // c.fillStyle = "rgba(0, 0, 255, 0.5)";
+    // Hitbox
+    // c.fillStyle = 'rgba(0, 0, 255, 0.5)'
     // c.fillRect(
     //   this.hitbox.x,
     //   this.hitbox.y,
     //   this.hitbox.width,
     //   this.hitbox.height,
-    // );
+    // )
 
     if (this.isImageLoaded === true) {
-      let xScale = 1;
-      let x = this.x;
+      let xScale = 1
+      let x = this.x
 
-      if (this.facing === "right") {
-        xScale = -1;
-        x = -this.x - this.width;
+      if (this.facing === 'right') {
+        xScale = -1
+        x = -this.x - this.width
       }
-      c.save;
-      c.scale(xScale, 1);
+
+      c.save()
+      c.scale(xScale, 1)
       c.drawImage(
         this.image,
         this.currentSprite.x + this.currentSprite.width * this.currentFrame,
@@ -75,88 +77,90 @@ class Oposum {
         x,
         this.y,
         this.width,
-        this.height
-      );
+        this.height,
+      )
+      c.restore()
     }
   }
 
   update(deltaTime, collisionBlocks) {
-    if (!deltaTime) return;
+    if (!deltaTime) return
 
     // Updating animation frames
-    this.elapsedTime += deltaTime;
-    const secondsInterval = 0.1;
+    this.elapsedTime += deltaTime
+    const secondsInterval = 0.1
     if (this.elapsedTime > secondsInterval) {
-      this.currentFrame = (this.currentFrame + 1) % this.currentSprite.frames;
-      this.elapsedTime -= secondsInterval;
+      this.currentFrame = (this.currentFrame + 1) % this.currentSprite.frames
+      this.elapsedTime -= secondsInterval
     }
 
     // Update hitbox position
-    this.hitbox.x = this.x;
-    this.hitbox.y = this.y + 9;
+    this.hitbox.x = this.x
+    this.hitbox.y = this.y + 9
 
-    this.applyGravity(deltaTime);
+    this.applyGravity(deltaTime)
 
     // Update horizontal position and check collisions
-    this.updateHorizontalPosition(deltaTime);
-    this.checkForHorizontalCollisions(collisionBlocks);
+    this.updateHorizontalPosition(deltaTime)
+    this.checkForHorizontalCollisions(collisionBlocks)
 
     // Check for any platform collisions
-    this.checkPlatformCollisions(platforms, deltaTime);
+    this.checkPlatformCollisions(platforms, deltaTime)
 
     // Update vertical position and check collisions
-    this.updateVerticalPosition(deltaTime);
-    this.checkForVerticalCollisions(collisionBlocks);
+    this.updateVerticalPosition(deltaTime)
+    this.checkForVerticalCollisions(collisionBlocks)
 
-    this.determineDirection();
+    this.determineDirection()
   }
 
   determineDirection() {
     if (this.velocity.x > 0) {
-      this.facing = "right";
+      this.facing = 'right'
     } else if (this.velocity.x < 0) {
-      this.facing = "left";
+      this.facing = 'left'
     }
   }
 
   jump() {
-    this.velocity.y = -OPOSUM_JUMP_POWER;
-    this.isOnGround = false;
+    this.velocity.y = -OPOSUM_JUMP_POWER
+    this.isOnGround = false
   }
 
   updateHorizontalPosition(deltaTime) {
     if (Math.abs(this.distanceTraveled) > this.turningDistance) {
-      this.velocity.x = -this.velocity.x;
-      this.distanceTraveled = 0;
+      this.velocity.x = -this.velocity.x
+      this.distanceTraveled = 0
     }
-    this.x += this.velocity.x * deltaTime;
-    this.hitbox.x += this.velocity.x * deltaTime;
-    this.distanceTraveled += this.velocity.x * deltaTime;
+
+    this.x += this.velocity.x * deltaTime
+    this.hitbox.x += this.velocity.x * deltaTime
+    this.distanceTraveled += this.velocity.x * deltaTime
   }
 
   updateVerticalPosition(deltaTime) {
-    this.y += this.velocity.y * deltaTime;
-    this.hitbox.y += this.velocity.y * deltaTime;
+    this.y += this.velocity.y * deltaTime
+    this.hitbox.y += this.velocity.y * deltaTime
   }
 
   applyGravity(deltaTime) {
-    this.velocity.y += OPOSUM_GRAVITY * deltaTime;
+    this.velocity.y += OPOSUM_GRAVITY * deltaTime
   }
 
   handleInput(keys) {
-    this.velocity.x = 0;
+    this.velocity.x = 0
 
     if (keys.d.pressed) {
-      this.velocity.x = OPOSUM_X_VELOCITY;
+      this.velocity.x = OPOSUM_X_VELOCITY
     } else if (keys.a.pressed) {
-      this.velocity.x = -OPOSUM_X_VELOCITY;
+      this.velocity.x = -OPOSUM_X_VELOCITY
     }
   }
 
   checkForHorizontalCollisions(collisionBlocks) {
-    const buffer = 0.0001;
+    const buffer = 0.0001
     for (let i = 0; i < collisionBlocks.length; i++) {
-      const collisionBlock = collisionBlocks[i];
+      const collisionBlock = collisionBlocks[i]
 
       // Check if a collision exists on all axes
       if (
@@ -167,25 +171,25 @@ class Oposum {
       ) {
         // Check collision while player is going left
         if (this.velocity.x < -0) {
-          this.hitbox.x = collisionBlock.x + collisionBlock.width + buffer;
-          this.x = this.hitbox.x;
-          break;
+          this.hitbox.x = collisionBlock.x + collisionBlock.width + buffer
+          this.x = this.hitbox.x
+          break
         }
 
         // Check collision while player is going right
         if (this.velocity.x > 0) {
-          this.hitbox.x = collisionBlock.x - this.hitbox.width - buffer;
-          this.x = this.hitbox.x;
-          break;
+          this.hitbox.x = collisionBlock.x - this.hitbox.width - buffer
+          this.x = this.hitbox.x
+          break
         }
       }
     }
   }
 
   checkForVerticalCollisions(collisionBlocks) {
-    const buffer = 0.0001;
+    const buffer = 0.0001
     for (let i = 0; i < collisionBlocks.length; i++) {
-      const collisionBlock = collisionBlocks[i];
+      const collisionBlock = collisionBlocks[i]
 
       // If a collision exists
       if (
@@ -196,34 +200,34 @@ class Oposum {
       ) {
         // Check collision while player is going up
         if (this.velocity.y < 0) {
-          this.velocity.y = 0;
-          this.hitbox.y = collisionBlock.y + collisionBlock.height + buffer;
-          this.y = this.hitbox.y - 9;
-          break;
+          this.velocity.y = 0
+          this.hitbox.y = collisionBlock.y + collisionBlock.height + buffer
+          this.y = this.hitbox.y - 9
+          break
         }
 
         // Check collision while player is going down
         if (this.velocity.y > 0) {
-          this.velocity.y = 0;
-          this.y = collisionBlock.y - this.height - buffer;
-          this.hitbox.y = collisionBlock.y - this.hitbox.height - buffer;
-          this.isOnGround = true;
-          break;
+          this.velocity.y = 0
+          this.y = collisionBlock.y - this.height - buffer
+          this.hitbox.y = collisionBlock.y - this.hitbox.height - buffer
+          this.isOnGround = true
+          break
         }
       }
     }
   }
 
   checkPlatformCollisions(platforms, deltaTime) {
-    const buffer = 0.0001;
+    const buffer = 0.0001
     for (let platform of platforms) {
       if (platform.checkCollision(this, deltaTime)) {
-        this.velocity.y = 0;
-        this.y = platform.y - this.height - buffer;
-        this.isOnGround = true;
-        return;
+        this.velocity.y = 0
+        this.y = platform.y - this.height - buffer
+        this.isOnGround = true
+        return
       }
     }
-    this.isOnGround = false;
+    this.isOnGround = false
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,46 +1,48 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-const dpr = window.devicePixelRatio || 1
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+const dpr = window.devicePixelRatio || 1;
 
-canvas.width = 1024 * dpr
-canvas.height = 576 * dpr
+canvas.width = 1024 * dpr;
+canvas.height = 576 * dpr;
 
 const oceanLayerDate = {
   l_New_Layer_1: l_New_Layer_1,
-}
+};
 
 const brambleLayerData = {
   l_Floresta: l_Floresta,
-}
+};
 const layersData = {
   //  l_New_Layer_1: l_New_Layer_1,
   //  l_Floresta: l_Floresta,
-   l_New_Layer_5: l_New_Layer_5,
-   l_Terreno: l_Terreno,
-   l_Casa: l_Casa,
-   l_Espinhos: l_Espinhos,
-   l_Itens: l_Itens,
-   l_Inimigos: l_Inimigos,
-   l_Colisores: l_Colisores,
+  l_New_Layer_5: l_New_Layer_5,
+  l_Terreno: l_Terreno,
+  l_Casa: l_Casa,
+  l_Espinhos: l_Espinhos,
+  l_Itens: l_Itens,
+  l_Inimigos: l_Inimigos,
+  l_Colisores: l_Colisores,
 };
 
 const tilesets = {
-  l_New_Layer_1: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_Floresta: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_New_Layer_5: { imageUrl: './images/tileset.png', tileSize: 16 },
-  l_Terreno: { imageUrl: './images/tileset.png', tileSize: 16 },
-  l_Casa: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_Espinhos: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_Itens: { imageUrl: './images/235acecb-61d8-4f9b-4f6c-8995c1930e00.png', tileSize: 16 },
-  l_Inimigos: { imageUrl: './images/decorations.png', tileSize: 16 },
-  l_Colisores: { imageUrl: './images/decorations.png', tileSize: 16 },
+  l_New_Layer_1: { imageUrl: "./images/decorations.png", tileSize: 16 },
+  l_Floresta: { imageUrl: "./images/decorations.png", tileSize: 16 },
+  l_New_Layer_5: { imageUrl: "./images/tileset.png", tileSize: 16 },
+  l_Terreno: { imageUrl: "./images/tileset.png", tileSize: 16 },
+  l_Casa: { imageUrl: "./images/decorations.png", tileSize: 16 },
+  l_Espinhos: { imageUrl: "./images/decorations.png", tileSize: 16 },
+  l_Itens: {
+    imageUrl: "./images/235acecb-61d8-4f9b-4f6c-8995c1930e00.png",
+    tileSize: 16,
+  },
+  l_Inimigos: { imageUrl: "./images/decorations.png", tileSize: 16 },
+  l_Colisores: { imageUrl: "./images/decorations.png", tileSize: 16 },
 };
 
-
 // Tile setup
-const collisionBlocks = []
-const platforms = []
-const blockSize = 16 // Assuming each tile is 16x16 pixels
+const collisionBlocks = [];
+const platforms = [];
+const blockSize = 16; // Assuming each tile is 16x16 pixels
 
 collisions.forEach((row, y) => {
   row.forEach((symbol, x) => {
@@ -50,8 +52,8 @@ collisions.forEach((row, y) => {
           x: x * blockSize,
           y: y * blockSize,
           size: blockSize,
-        }),
-      )
+        })
+      );
     } else if (symbol === 2) {
       platforms.push(
         new Platform({
@@ -59,26 +61,26 @@ collisions.forEach((row, y) => {
           y: y * blockSize + blockSize,
           width: 16,
           height: 4,
-        }),
-      )
+        })
+      );
     }
-  })
-})
+  });
+});
 
 const renderLayer = (tilesData, tilesetImage, tileSize, context) => {
   // Calculate the number of tiles per row in the tileset
   // We use Math.ceil to ensure we get a whole number of tiles
-  const tilesPerRow = Math.ceil(tilesetImage.width / tileSize)
+  const tilesPerRow = Math.ceil(tilesetImage.width / tileSize);
 
   tilesData.forEach((row, y) => {
     row.forEach((symbol, x) => {
       if (symbol !== 0) {
         // Adjust index to be 0-based for calculations
-        const tileIndex = symbol - 1
+        const tileIndex = symbol - 1;
 
         // Calculate source coordinates
-        const srcX = (tileIndex % tilesPerRow) * tileSize
-        const srcY = Math.floor(tileIndex / tilesPerRow) * tileSize
+        const srcX = (tileIndex % tilesPerRow) * tileSize;
+        const srcY = Math.floor(tileIndex / tilesPerRow) * tileSize;
 
         context.drawImage(
           tilesetImage, // source image
@@ -89,31 +91,31 @@ const renderLayer = (tilesData, tilesetImage, tileSize, context) => {
           x * 16,
           y * 16, // destination x, y
           16,
-          16, // destination width, height
-        )
+          16 // destination width, height
+        );
       }
-    })
-  })
-}
+    });
+  });
+};
 const renderStaticLayers = async (layersData) => {
-  const offscreenCanvas = document.createElement('canvas')
-  offscreenCanvas.width = canvas.width
-  offscreenCanvas.height = canvas.height
-  const offscreenContext = offscreenCanvas.getContext('2d')
+  const offscreenCanvas = document.createElement("canvas");
+  offscreenCanvas.width = canvas.width;
+  offscreenCanvas.height = canvas.height;
+  const offscreenContext = offscreenCanvas.getContext("2d");
 
   for (const [layerName, tilesData] of Object.entries(layersData)) {
-    const tilesetInfo = tilesets[layerName]
+    const tilesetInfo = tilesets[layerName];
     if (tilesetInfo) {
       try {
-        const tilesetImage = await loadImage(tilesetInfo.imageUrl)
+        const tilesetImage = await loadImage(tilesetInfo.imageUrl);
         renderLayer(
           tilesData,
           tilesetImage,
           tilesetInfo.tileSize,
-          offscreenContext,
-        )
+          offscreenContext
+        );
       } catch (error) {
-        console.error(`Failed to load image for layer ${layerName}:`, error)
+        console.error(`Failed to load image for layer ${layerName}:`, error);
       }
     }
   }
@@ -122,8 +124,8 @@ const renderStaticLayers = async (layersData) => {
   // collisionBlocks.forEach(block => block.draw(offscreenContext));
   // platforms.forEach((platform) => platform.draw(offscreenContext))
 
-  return offscreenCanvas
-}
+  return offscreenCanvas;
+};
 // END - Tile setup
 
 // Change xy coordinates to move player's default position
@@ -132,15 +134,51 @@ const player = new Player({
   y: 751,
   size: 32,
   velocity: { x: 0, y: 0 },
-})
+});
 
-const oposum = new Oposum({
-  x: 200,
-  y: 751,
-  size: 32,
-})
+let oposums = []
 
+oposums = [
+  new Oposum({
+    x: 200,
+    y: 751,
+    width: 32,
+    height: 32,
+  }),
+  new Oposum({
+    x: 400,
+    y: 751,
+    width: 32,
+    height: 32,
+  }),
+  new Oposum({
+    x: 600,
+    y: 751,
+    width: 32,
+    height: 32,
+  }),
+  new Oposum({
+    x: 800,
+    y: 751,
+    width: 32,
+    height: 32,
+  }),
+  new Oposum({
+    x: 1000,
+    y: 751,
+    width: 32,
+    height: 32,
+  }),
+  new Oposum({
+    x: 1200,
+    y: 751,
+    width: 32,
+    height: 32,
+  }),
 
+]
+
+const sprites = [];
 
 const keys = {
   w: {
@@ -152,92 +190,132 @@ const keys = {
   d: {
     pressed: false,
   },
-}
+};
 
-let lastTime = performance.now()
+let lastTime = performance.now();
 
-const SCROLL_POST_RIGHT = 300
-const SCROLL_POST_LEFT = 635
-const SCROLL_POST_TOP = 350
-const SCROLL_POST_BOTTOM = 527
+const SCROLL_POST_RIGHT = 300;
+const SCROLL_POST_LEFT = 635;
+const SCROLL_POST_TOP = 350;
+const SCROLL_POST_BOTTOM = 530;
 
-let oceanBackgroundCanvas = null
-let brambleBackgroundCanvas = null
+let oceanBackgroundCanvas = null;
+let brambleBackgroundCanvas = null;
 
 const camera = {
   x: 0,
   y: 0,
-}
-
+};
 
 function animate(backgroundCanvas) {
   // Calculate delta time
-  const currentTime = performance.now()
-  const deltaTime = (currentTime - lastTime) / 1000
-  lastTime = currentTime
+  const currentTime = performance.now();
+  const deltaTime = (currentTime - lastTime) / 1000;
+  lastTime = currentTime;
 
   // Update player position
-  player.handleInput(keys)
-  player.update(deltaTime, collisionBlocks)
+  player.handleInput(keys);
+  player.update(deltaTime, collisionBlocks);
 
   // Update oposum position
-  oposum.update(deltaTime, collisionBlocks)
+  for (let i = oposums.length - 1; i >= 0; i--) {
+    const oposum = oposums[i]
+    oposum.update(deltaTime, collisionBlocks)
 
-  if (checkCollisios(player, oposum)) {
-    player.velocity.y = -200
+    // Jump on enemy
+    if (checkCollisios(player, oposum)) {
+      player.velocity.y = -200
+      sprites.push (
+        new Sprite({
+          x: oposum.x,
+          y: oposum.y,
+          width: 32,
+          height: 32,
+          imageSrc: "../images/enemy-deadth.png",
+          spriteCropbox: {
+            x: 0,
+            y: 0,
+            width: 40,
+            height: 41,
+            frames: 6 
+          },
+        }),
+
+      )
+
+      oposums.splice(i, 1)
+    }
   }
+  for (let i = sprites.length - 1; i >= 0; i--) {
+    const sprite = sprites[i];
+    sprite.update(deltaTime);
+
+    if (sprite.iteration === 1) {
+      sprites.splice(i, 1)
+    }
+  }
+
+  
 
   // Track scroll post distance
   if (player.x > SCROLL_POST_RIGHT && player.x < SCROLL_POST_LEFT) {
-    const scrollPostDistance = player.x - SCROLL_POST_RIGHT
-    camera.x = scrollPostDistance
+    const scrollPostDistance = player.x - SCROLL_POST_RIGHT;
+    camera.x = scrollPostDistance;
   }
 
   if (player.y < SCROLL_POST_TOP && camera.y > 0) {
-    const scrollPostDistance = SCROLL_POST_TOP - player.y
-    camera.y = scrollPostDistance
+    const scrollPostDistance = SCROLL_POST_TOP - player.y;
+    camera.y = scrollPostDistance;
   }
 
   if (player.y > SCROLL_POST_BOTTOM) {
-    const scrollPostDistance = player.y - SCROLL_POST_BOTTOM  
-    camera.y = - scrollPostDistance
+    const scrollPostDistance = player.y - SCROLL_POST_BOTTOM;
+    camera.y = -scrollPostDistance;
   }
 
   // Render scene
-  c.save()
-  c.scale(dpr, dpr)
-  c.translate(-camera.x, camera.y)
-  c.clearRect(0, 0, canvas.width, canvas.height)
-  c.drawImage(oceanBackgroundCanvas, camera.x *0.32, 0)
-  c.drawImage(brambleBackgroundCanvas, camera.x *0.16, 0)
-  c.drawImage(backgroundCanvas, 0, 0)
+  c.save();
+  c.scale(dpr, dpr);
+  c.translate(-camera.x, camera.y);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.drawImage(oceanBackgroundCanvas, camera.x * 0.32, 0);
+  c.drawImage(brambleBackgroundCanvas, camera.x * 0.16, 0);
+  c.drawImage(backgroundCanvas, 0, 0);
   // c.fillStyle = 'rgba(255, 0, 0, 0.5)'
   // c.fillRect(SCROLL_POST_RIGHT, 0, 10, 1000)
   // c.fillRect(SCROLL_POST_LEFT, 0, 10, 1000)
   // c.fillRect(0, SCROLL_POST_TOP, 1000, 10)
   // c.fillRect(0, SCROLL_POST_BOTTOM, 1000, 10)
-  player.draw(c)
-  oposum.draw(c)
-  c.restore()
+  player.draw(c);
 
-  requestAnimationFrame(() => animate(backgroundCanvas))
+  for (let i = oposums.length - 1; i >= 0; i--) {
+    const oposum = oposums[i]
+    oposum.draw(c)
+  }
+
+  for (let i = sprites.length - 1; i >= 0; i--) {
+    const sprite = sprites[i]
+    sprite.draw(c)
+  }
+  c.restore();
+
+  requestAnimationFrame(() => animate(backgroundCanvas));
 }
 
 const startRendering = async () => {
   try {
-    oceanBackgroundCanvas = await renderStaticLayers(oceanLayerDate)
-    brambleBackgroundCanvas = await renderStaticLayers(brambleLayerData)
-    const backgroundCanvas = await renderStaticLayers(layersData)
+    oceanBackgroundCanvas = await renderStaticLayers(oceanLayerDate);
+    brambleBackgroundCanvas = await renderStaticLayers(brambleLayerData);
+    const backgroundCanvas = await renderStaticLayers(layersData);
     if (!backgroundCanvas) {
-      console.error('Failed to create the background canvas')
-      return
+      console.error("Failed to create the background canvas");
+      return;
     }
 
-    animate(backgroundCanvas)
+    animate(backgroundCanvas);
   } catch (error) {
-    console.error('Error during rendering:', error)
+    console.error("Error during rendering:", error);
   }
-}
+};
 
-startRendering()
-
+startRendering();
